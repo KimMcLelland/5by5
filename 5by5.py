8e42a319fca21369563a8d9c7606fa5b8f538427
Kim <faeriekim3@gmail.com>
2021-05-08 16:01:16 +0100

    added more sprites etc

--- 5by5.py
+++ 5by5.py
@@ -116,16 +116,63 @@ screen25 = tropicalSelector()
 
 tropicalZone = [screen21, screen22, screen23, screen24, screen25]
 
+current_screen = screen11
+
 GREEN = (124, 252, 0)
 
 FPS = 60
 
 player_character_image = pygame.image.load("assets/images/5by5chr.png")
+mob1_image = pygame.image.load("assets/images/Sheep.png")
+mob2_image = pygame.image.load("assets/images/cow.png")
+resource1_image = pygame.image.load("assets/images/scrub.png")
+resource2_image = pygame.image.load("assets/images/quarry.png")
+resource3_image = pygame.image.load("assets/images/lake.png")
 
-def draw_window(player):
-    WIN.fill((GREEN))
+def draw_window(player, sheep, cow, quarry, shrub, lake):
+    if current_screen == "Plains":
+        WIN.fill((GREEN))
+        WIN.blit(mob1_image, (sheep.x, sheep.y))
+        WIN.blit(mob2_image, (cow.x, cow.y))
+        WIN.blit(resource1_image, (shrub.x, shrub.y))
+        WIN.blit(resource2_image, (quarry.x, quarry.y))
+        WIN.blit(resource3_image, (lake.x, lake.y))
     WIN.blit(player_character_image, (player.x, player.y)) 
+    
     pygame.display.update()
+
+def player_handle_movement(player, keys_pressed):
+    if keys_pressed[pygame.K_LEFT] and player.x-1 > 0:
+        player.x -= 5
+    if keys_pressed[pygame.K_RIGHT] and player.x+1 < 850:
+        player.x += 5
+    if keys_pressed[pygame.K_UP] and player.y-1 > 0:
+        player.y -= 5
+    if keys_pressed[pygame.K_DOWN] and player.y+1 < 450:
+        player.y += 5
+
+def handle_sheep_movement(sheep):
+    num = random.randint(1, 4)
+    if num == 1 and sheep.x-1: 
+        sheep.x -= 2
+    if num == 2 and sheep.x+1 < 850: 
+        sheep.x += 2
+    if num == 3 and sheep.y-1 > 0: 
+        sheep.y -= 2
+    if num == 4 and sheep.y+1 < 450:
+        sheep.y += 2
+
+def handle_cow_movement(cow):
+    num = random.randint(1, 4)
+    if num == 1 and cow.x-1: 
+        cow.x -= 2
+    if num == 2 and cow.x+1 < 850: 
+        cow.x += 2
+    if num == 3 and cow.y-1 > 0: 
+        cow.y -= 2
+    if num == 4 and cow.y+1 < 450:
+        cow.y += 2
+
 
 
 WIDTH, HEIGHT = 900, 500
@@ -133,6 +180,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("5 by 5")
 def main():
     player = pygame.Rect(0, 200, 50, 50)
+    cow = pygame.Rect(400, 100, 50, 50)
+    sheep = pygame.Rect(700, 400, 50, 50)
+    quarry = pygame.Rect(200, 300, 50, 50)
+    shrub = pygame.Rect(400, 300, 50, 50)
+    lake = pygame.Rect(600, 100, 50, 50)
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -142,15 +194,10 @@ def main():
                 run = False
         
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_LEFT]:
-            player.x -= 5
-        if keys_pressed[pygame.K_RIGHT]:
-            player.x += 5
-        if keys_pressed[pygame.K_UP]:
-            player.y -= 5
-        if keys_pressed[pygame.K_DOWN]:
-            player.y += 5
-        draw_window(player)
+        player_handle_movement(player, keys_pressed)
+        handle_sheep_movement(sheep)
+        handle_cow_movement(cow)
+        draw_window(player, sheep, cow, quarry, shrub, lake)
 
     pygame.quit()
 
